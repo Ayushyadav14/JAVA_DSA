695b35516bbd3d27739da54e0c5cbe0c05b64280
Ayush Yadav <139036649+Ayushyadav14@users.noreply.github.com>
2026-06-24 10:58:09 +0530

    678. Valid Parenthesis String

---
+++ src/Leetcode/678.ValidParenthesisString.java
@@ -0,0 +1,27 @@
+//678. Valid Parenthesis String
+class Solution {
+    public boolean checkValidString(String s) {
+        int n = s.length();
+        int min =0;
+        int max =0;
+        
+        for(int i =0;i<n;i++){
+            if(s.charAt(i) == '('){
+                min = min+1;
+                max = max+1;
+            }
+            else if(s.charAt(i) == ')'){
+                min = min-1;
+                max = max-1;
+            }
+            else{
+                min = min-1;
+                max = max+1;
+            }
+            if(max <0) return false;
+            if(min <0) min=0;
+        }
+        return min ==0;
+
+    }
+}
